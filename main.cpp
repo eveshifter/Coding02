0b74396c9b1a62502b50e23dc6443581cf18cc9b
eveshifter <alexandra.hammon@g.austincc.edu>
2021-09-25 15:59:48 -0500

    added while loop for parameter length

--- main.cpp
+++ main.cpp
@@ -5,18 +5,32 @@
 #include "main.h"
 
 int main(int argc, char** argv) {
+
     cout << "My Command line anazyler..." << endl;
+
+    //test and report the number of command line parameters
     cout << "The number of command line parameters are: " << argc << endl;
     
+    //test and report the length of the program name
     int count = 0;
     while(argv[0][count] != '\0'){
         count++;
     }
     cout << "The length of the name of the program is " << count << endl;
     
+    //loop through and report the length of each command line parameter except the name
     if (argc > 1){
-    	std::cout << "The length of the program is " << string_length(argv[0]) << std::endl;
+    
+    	
+    	for(int i=1; i<argc; i++){
+    		int length = 0;
+    		while(argv[i][length] != '\0'){
+        	length++;
     	}
+            cout << "The length of the parameter " << i << " is " << length << endl;
+    	}
+  
+    }
 
     return 0;
 }
