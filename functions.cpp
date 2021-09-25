22adbf55499550d094fef14150388600c03e272b
eveshifter <alexandra.hammon@g.austincc.edu>
2021-09-25 13:36:54 -0500

    added string_length function

--- functions.cpp
+++ functions.cpp
@@ -1,3 +1,11 @@
 /*********************
 * Your comment header here.
 **********************/
+
+#include "functions.h"
+
+//string_length definition.
+int string_length(const char *str){
+
+	return 0;
+}
--- functions.h
+++ functions.h
@@ -5,6 +5,8 @@
 #ifndef COMMANDLINE_FUNCTIONS_H
 #define COMMANDLINE_FUNCTIONS_H
 
+//string_length prototype.
+int string_length(const char*);
 
 
 #endif //COMMANDLINE_FUNCTIONS_H
