83dd417eb90a73e0f27b38e5744eb6139c210d89
eveshifter <alexandra.hammon@g.austincc.edu>
2021-09-25 15:19:37 -0500

    added while loop for name length

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@ int main(int argc, char** argv) {
     cout << "My Command line anazyler..." << endl;
     cout << "The number of command line parameters are: " << argc << endl;
     
+    int count = 0;
+    while(argv[0][count] != '\0'){
+        count++;
+    }
+    cout << "The length of the name of the program is " << count << endl;
+    
     if (argc > 1){
-    	std::cout << "The length of the first parameter is " << string_length(argv[1]) << std::endl;
+    	std::cout << "The length of the program is " << string_length(argv[0]) << std::endl;
     	}
 
     return 0;
